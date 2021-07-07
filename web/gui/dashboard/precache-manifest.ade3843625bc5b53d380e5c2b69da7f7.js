2a6aa27ce23ae2c8f6e33e68913a3444facd19a1
Pantelis P <p.paliagkas@gmail.com>
2021-07-07 12:53:36 +0300

    v2.18.5 (#11316)

--- web/gui/dashboard/precache-manifest.ade3843625bc5b53d380e5c2b69da7f7.js
+++ web/gui/dashboard/precache-manifest.ade3843625bc5b53d380e5c2b69da7f7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd8b3eec0547712be881e2268340c188",
+    "revision": "a97d15820d024cdf0d76193604870ef4",
     "url": "./index.html"
   },
   {
-    "revision": "1d9e7a7bce708f2c871c",
+    "revision": "6cc934e435364fc9dcf7",
     "url": "./static/css/2.6b842ba1.chunk.css"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/4.a36e3b73.chunk.css"
   },
   {
-    "revision": "7ca029cb4a28e733f130",
+    "revision": "6de5f2135b8f36fa27a1",
     "url": "./static/css/main.d931154a.chunk.css"
   },
   {
@@ -20,12 +20,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/10.db7e8e19.chunk.js"
   },
   {
-    "revision": "1d9e7a7bce708f2c871c",
-    "url": "./static/js/2.252b3a57.chunk.js"
+    "revision": "6cc934e435364fc9dcf7",
+    "url": "./static/js/2.3cc24b0f.chunk.js"
   },
   {
     "revision": "3566dd793a8b2b43e63ae817a0e9f56c",
-    "url": "./static/js/2.252b3a57.chunk.js.LICENSE"
+    "url": "./static/js/2.3cc24b0f.chunk.js.LICENSE"
   },
   {
     "revision": "728ef1c58faa5d9b014f",
@@ -60,12 +60,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/9.e3a9ce26.chunk.js"
   },
   {
-    "revision": "7ca029cb4a28e733f130",
-    "url": "./static/js/main.8aa70c75.chunk.js"
+    "revision": "6de5f2135b8f36fa27a1",
+    "url": "./static/js/main.98b89fce.chunk.js"
   },
   {
     "revision": "19356475904bddb45614eb6ff7f6cd44",
-    "url": "./static/js/main.8aa70c75.chunk.js.LICENSE"
+    "url": "./static/js/main.98b89fce.chunk.js.LICENSE"
   },
   {
     "revision": "4c114e675b21d38feff3",
